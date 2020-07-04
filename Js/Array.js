8ee9a699b6bb9019451901fa56027423c42fc10b
Yajun(Maggie) Wan <yajunwan@users.noreply.github.com>
2020-07-03 23:38:15 -0700

    Create Array.js

---
+++ Js/Array.js
@@ -0,0 +1,32 @@
+//Manipulate arrays
+//Array Maximum
+const myArray1 = [3,11,7,2,9,10];
+let max = myArray1[0];
+for (const num of myArray1) {
+    if (num>max){
+        max = num;
+    } 
+}
+console.log(max);
+
+//List of words
+const myArray2 = [];
+const words = () =>{
+    flag=true;
+    while(flag){
+        let user_input = prompt("Please enter a word, type 'stop' to exit");
+        if (user_input == "stop") {
+            flag = false;
+        }
+        else{
+            myArray2.push(user_input);
+        }
+    }
+    console.log(myArray2);
+};
+words();
+
+
+
+
+
