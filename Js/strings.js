73eb5be5e0688dc5dacdb0d87551dafc49c1b07d
Yajun(Maggie) Wan <yajunwan@users.noreply.github.com>
2020-07-04 17:44:08 -0700

    Create strings.js

---
+++ Js/strings.js
@@ -0,0 +1,26 @@
+//Work with strings
+//Word indo
+const myStr = prompt("Please enter a string");
+alert("The length of the string you entered is: "+myStr.length);
+alert(`The lowercase of the string is ${myStr.toLowerCase()}, and the uppercase of that is ${myStr.toUpperCase()}`);
+
+//Vowel count
+const strArr = Array.from(myStr);
+let count = 0;
+strArr.forEach(letters => {
+    if ("aeiou".indexOf(letters) !== -1){
+        count++
+    }
+});
+alert(`There are ${count} vowels in the string`);
+
+//Backwards word
+function reverse(str){
+    if (str == ""){
+        return "";
+    }
+    else{
+        return reverse(str.substr(1))+str.charAt(0);
+    }
+}
+alert(reverse(myStr));
